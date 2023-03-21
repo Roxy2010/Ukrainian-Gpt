649500bb0e9dd34cd15ba3a6876e3a96da2ed41c
Roxy <kazarova.roksana@gmail.com>
2023-03-21 19:45:38 +0100

    add openai api

--- telegramm gpt.py
+++ telegramm gpt.py
@@ -1,3 +1,21 @@
+import openai
+API_KEY = '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'
+model_id = 'gpt-3.5-turbo'
+
+def init_openAI_API_Key():
+    openai.api_key = API_KEY
+
+def Send(message):
+    myMessage = [] 
+    myMessage.append({'role': 'system', 'content': message})
+    response = openai.ChatCompletion.create(
+        model =model_id,
+        messages=myMessage
+    )
+    return response
+
+init_openAI_API_Key()
 print("chat bot is start")
 text= input(" ")
-print(f"{text}")
+response=Send(text)
+print(response['choices'][0].message.content)
