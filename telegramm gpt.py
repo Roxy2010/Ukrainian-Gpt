6a249d874a9172a5f5cb2d1a7e2e3f28b7d6c76d
Roxy <kazarova.roksana@gmail.com>
2023-03-22 19:21:20 +0100

    add telegramm api

--- telegramm gpt.py
+++ telegramm gpt.py
@@ -1,9 +1,27 @@
 import openai
+import telebot
+
+bot=telebot.TeleBot('6167495629:AAGzGE1--4acZwUwIOIzvU1hjJEGX9xkyC8')
+
 API_KEY = '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'
 model_id = 'gpt-3.5-turbo'
 
 def init_openAI_API_Key():
     openai.api_key = API_KEY
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    send_message_to_user(message.chat.id, "Вас вітає Ukrainian GPT!Ви можете запитати в мене будь що чи просто поспілкуватися зі мною")
+
+@bot.message_handler(content_types=['text'])
+def handler_text(message):
+    send_message_to_user(message.chat.id, message.text)
+    
+
+
+def send_message_to_user(id, text):
+    bot.send_message(id, text)
+
 
 def Send(message):
     myMessage = [] 
@@ -16,6 +34,8 @@ def Send(message):
 
 init_openAI_API_Key()
 print("chat bot is start")
-text= input(" ")
+text= input("")
 response=Send(text)
 print(response['choices'][0].message.content)
+
+bot.polling(non_stop=True)
